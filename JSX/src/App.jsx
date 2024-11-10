7fc2771714a729fbc5b62e62a9b8526525fd8bf4
Firezer27 <firezer.bizuwork27@gmail.com>
2024-11-10 06:37:39 +0300

    apply jsx

---
+++ JSX/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import WelcomeMessage from './component/WelcomeMessage'
+
+function App() {
+
+  return (
+    <>
+      <WelcomeMessage/>
+    </>
+  )
+}
+
+export default App
